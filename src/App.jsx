13dc0d21208a0427a49530c4662706c1a3935847
Laz Austin <lazaustin94@gmail.com>
2022-02-28 10:30:21 -0600

    cloned MUI Blog templates header because it's the style I want for my final product, but havent figured out how some of the props work from the clone so Ive got them commented out.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Home } from '@mui/icons-material';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './header';
+
+
+function App() {
+  return (
+    <div className="App">
+
+        <Header />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/header.jsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+
+function Header() {
+  
+  //original clone from GitHub had const {sections, title} = props under function Header(props) and props as a parameter for Header; Ive commented out the section portion of the code below until I can figure out what it is for. For now it doesnt seem to affect the code at all.
+  return (
+    <React.Fragment>
+      <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Button size="small">Subscribe</Button>
+        <Typography
+          component="h2"
+          variant="h5"
+          color="inherit"
+          align="center"
+          noWrap
+          sx={{ flex: 1 }}
+        >
+          {/*original clone had title in curly braces right here instead of Blog like i have below}*/}
+          Blog
+        </Typography>
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <Button variant="outlined" size="small">
+          Sign up
+        </Button>
+      </Toolbar>
+      {/*<Toolbar
+        component="nav"
+        variant="dense"
+        sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
+      >
+        {sections.map((section) => (
+          <Link
+            color="inherit"
+            noWrap
+            key={section.title}
+            variant="body2"
+            href={section.url}
+            sx={{ p: 1, flexShrink: 0 }}
+          >
+            {section.title}
+          </Link>
+        ))}
+      </Toolbar>*/}
+    </React.Fragment>
+  );
+}
+
+Header.propTypes = {
+  sections: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
