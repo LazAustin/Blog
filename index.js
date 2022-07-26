4cb56334f76e51ab9152107eec86537810a0d61d
Laz Austin <lazaustin94@gmail.com>
2022-07-26 16:18:15 -0500

    first deployment

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static('client/build'))  // set static folder 
   
-  app.get('*',(req,res)=>{     
-    res.sendFile (path.resolve(__dirname,'client','build',         
+  app.get('*', (req,res)=> {     
+    res.sendFile(path.resolve(__dirname,'client','build',         
                   'index.html' )); 
   })
 }
