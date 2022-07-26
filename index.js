b34487a249f9aa787ed48a56b4b121a91b8b624f
Laz Austin <lazaustin94@gmail.com>
2022-07-26 15:08:37 -0500

    first deployment

--- index.js
+++ index.js
@@ -47,11 +47,12 @@ app.use("/api/categories", categoryRoute);
 
 //Serve Front End
 if (process.env.NODE_ENV === "production") {
-    // Step 1:
-  app.use(express.static(path.resolve(__dirname, "./client/build")));
-  // Step 2:
-  app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+
+  app.use(express.static('client/build'))  // set static folder 
+  
+  app.get('*',(req,res)=>{     
+    res.sendFile (path.resolve(__dirname,'client','build',         
+                  'index.html' )); 
   })
 }
 
