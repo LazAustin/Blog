bbed69c1261c16dbb4c004a4d275e940fc6683fb
Laz Austin <lazaustin94@gmail.com>
2022-07-27 18:12:54 -0500

    first deployment

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
 
-app.use("/auth", authRoute);
-app.use("/users", userRoute);
-app.use("/posts", postRoute);
-app.use("/categories", categoryRoute);
+app.use("api/auth", authRoute);
+app.use("api/users", userRoute);
+app.use("api/posts", postRoute);
+app.use("api/categories", categoryRoute);
 
 //Serve Front End
 if (process.env.NODE_ENV === "production") {
