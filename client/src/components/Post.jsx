ae2305256b470af9aa5ee99f80972734317fa3c2
Laz Austin <lazaustin94@gmail.com>
2025-10-03 19:58:52 -0500

    testing new host & redeploy

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-import parser from 'html-react-parser'
+import parser from 'html-react-parser';
+import React from "react";
 
 export default function Post({ post }) {
 
